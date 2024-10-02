1dda39ff98752c0c3b37b0419e8c7c38c17701a5
Dongliang Li <dongliang14@gmail.com>
2024-10-02 07:43:33 -0700

    first commit

---
+++ client/app/page.js
@@ -0,0 +1,7 @@
+const Page = () => {
+  return(
+    <h1>Welcome to Home Page</h1>
+  );
+};
+
+export default Page;
---
+++ client/app/privacypolicy/page.js
@@ -0,0 +1,25 @@
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>Effective Date: </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porttitor lectus in elit feugiat bibendum. Nulla tortor felis, eleifend eu convallis in, vestibulum sit amet ante. Etiam vulputate sapien felis, vel sollicitudin arcu tincidunt a. Vestibulum pellentesque, felis in euismod maximus, diam dui euismod nunc, non porta purus nisl id urna. Curabitur pellentesque felis non diam venenatis, id mattis ante convallis. Suspendisse potenti. Proin molestie sit amet elit tempus tristique. Ut nulla risus, aliquet vel ligula et, convallis efficitur diam. Morbi eleifend ex a sagittis euismod. Etiam velit felis, volutpat non sagittis molestie, euismod et ante. Vivamus eu feugiat sapien. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque eros neque, sagittis congue dolor nec, facilisis cursus eros. Aenean eu venenatis eros. Quisque ac risus eget arcu volutpat consectetur. Cras ac ligula porta dui vulputate pretium.
+      </p>
+      <p>
+        Sed auctor, ipsum sed dictum pretium, mauris nunc tempus urna, quis tempor libero nibh ac mauris. Aenean aliquam enim nulla. Nunc porttitor malesuada risus, in pulvinar libero elementum vitae. Fusce purus felis, volutpat ac pellentesque eu, commodo sit amet sapien. Aliquam ut lorem a eros pretium laoreet vel ac ante. Aenean rutrum augue tempor eros mollis, at efficitur nunc vulputate. Aenean maximus auctor ante vitae pretium. Vestibulum pretium interdum eros, vitae finibus sem volutpat vitae. Aliquam vulputate pulvinar tortor, et pharetra dolor sodales nec.
+      </p>
+      <p>
+        Quisque tincidunt urna non tellus porta dapibus. Etiam ut ante at nisl varius sagittis sed a nisi. Aliquam erat volutpat. Fusce porta, justo imperdiet rutrum semper, odio erat congue ipsum, eu porta augue erat id ligula. Nunc pellentesque posuere commodo. Aliquam vestibulum nunc volutpat sapien vehicula accumsan. Aliquam dapibus, sem in fringilla vehicula, magna mi laoreet risus, consectetur vulputate ante tortor eget mauris. Morbi condimentum augue eu euismod condimentum.
+      </p>
+      <p>
+        Aenean a condimentum lectus, sit amet sodales mi. In ut consequat erat. Vestibulum sit amet lacus a dui volutpat eleifend in nec lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut aliquet risus ut nibh ultricies, nec imperdiet ante auctor. Suspendisse gravida congue massa vitae elementum. Nulla elit nibh, dapibus sed iaculis id, pretium euismod elit. Nullam finibus tristique sodales.
+      </p>
+      <p>
+        Curabitur hendrerit enim a porttitor rhoncus. Donec eu porta est, ut lacinia felis. Curabitur ultricies augue vitae tempus commodo. Sed mattis convallis congue. Duis luctus ante ac risus pharetra tempus. Pellentesque et enim et felis euismod faucibus at vitae arcu. Etiam ut tristique dui, eu efficitur libero. Aenean et mollis ipsum, non pellentesque tortor. Suspendisse vehicula vitae purus ut viverra. Aenean imperdiet quam et arcu venenatis, eget venenatis eros volutpat. Donec vel risus non est interdum tincidunt. Nullam placerat ut elit quis finibus. Morbi nec condimentum leo.
+      </p>
+    </div>
+  )
+}
+
+export default PrivacyPolicy
